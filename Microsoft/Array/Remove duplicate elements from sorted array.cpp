3a370f8395c1bc100f632ea317e2375da7c1b10b
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-30 15:56:03 +0530

    Update and rename Question.cpp to Remove duplicate elements from sorted array.cpp

---
+++ Microsoft/Array/Remove duplicate elements from sorted array.cpp
@@ -0,0 +1,16 @@
+// https://practice.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/0/?track=mts-array&batchId=341#
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        int res = 1;
+        for(int i=1; i<n; i++){
+        if(a[res-1]!=a[i]){
+        a[res] = a[i];
+        res++;
+        }
+    }
+        return res;
+    }
+    
+};
