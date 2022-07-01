3636363e92120f6c59e3bb3cfd64261d7383719c
Chirantan Banik <banikchirantan16@gmail.com>
2022-07-01 19:24:46 +0530

    Update Josephus.cpp

--- Companies/Recursion/Josephus.cpp
+++ Companies/Recursion/Josephus.cpp
@@ -1,2 +1,18 @@
 // Amazon
 
+// https://practice.geeksforgeeks.org/problems/josephus-problem/0/?track=DSASP-Recursion&batchId=154#
+
+class Solution
+{
+    public:
+    int josephus(int n, int k)
+    {
+       if(n==1)
+       return 1;
+       
+       else 
+       
+       return (josephus(n-1, k) + k-1) % n+1;
+    }
+
+};
